f97f8f89e521dc9b4219f9f6d01a8315f51d0d22
Robyn Green <robyn.green@mediacurrent.com>
2017-01-08 23:04:50 -0600

    Ignored bower + gulp config

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/*eslint strict: ["error", "global"]*/
+'use strict';
+
+//=======================================================
+// Include gulp
+//=======================================================
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+var config = {
+    bootstrapDir: './bower_components/bootstrap-sass',
+    publicDir: './public',
+};
+
+gulp.task('css', function() {
+    return gulp.src('./css/app.scss')
+    .pipe(sass({
+        includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+    }))
+    .pipe(gulp.dest(config.publicDir + '/css'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+    .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('default', ['css', 'fonts']);
