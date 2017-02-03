7f45d6fe57e014f7c3de13480863bd46d9b6e44c
Robyn Green <robyn.green@mediacurrent.com>
2017-02-03 08:26:24 -0600

    Added google maps npm

--- app/maps.js
+++ app/maps.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from "react";
+import GoogleMap from 'google-map-react';
 
 var radarSRC = 'http://api.wunderground.com/api/13d3adca9dd11d63/animatedradar/q/AR/Conway.png?width=500&height=250&newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50&smooth=1&noclutter=1&rainsnow=1';
 
@@ -9,6 +10,15 @@ var radarSRC = 'http://api.wunderground.com/api/13d3adca9dd11d63/animatedradar/q
 // lat: 34.8415512, lng: -92.3331572
 
 export default React.createClass({
+   /**
+   * Sets up our variables.
+   */
+  getInitialState: function() {
+    return {
+      center: {lat: 34.8915512, lng: -92.3031572},
+      zoom: 9
+    }
+  },
   render: function() {
     var radarIMG = React.createElement(
       'img',
@@ -19,7 +29,16 @@ export default React.createClass({
         <div className="weather-map map">
           { radarIMG }
         </div>
-        <div id="trafficMap" className="traffic-map map"></div>
+
+        <div className="traffic-map map">
+          <GoogleMap
+            bootstrapURLKeys={{ key: "AIzaSyDOzStmGXlCBYxgCKLPruGgqQGGxIfnIaI" }}
+            className="traffic-map map"
+            defaultCenter={this.state.center}
+            defaultZoom={this.state.zoom}
+            layerTypes={['TrafficLayer']}>
+          </GoogleMap>
+        </div>
       </div>
     );
   }
